09f1a813571b4c04cf7be73710a7026594cd878d
Roman Rachugin <rro1699@mail.ru>
2022-06-12 21:45:38 +0300

    TgBot-1: added simple telegram bot to project

---
+++ src/main/java/com/example/telegrambot/bot/SmokyJavaBot.java
@@ -0,0 +1,44 @@
+package com.example.telegrambot.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class SmokyJavaBot extends TelegramLongPollingBot {
+
+    @Value("${bot.username}")
+    private String username;
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()){
+            String message = update.getMessage().getText().trim();
+            String chatId = update.getMessage().getChatId().toString();
+            SendMessage message1 = new SendMessage();
+            message1.setChatId(chatId);
+            message1.setText(message);
+            try {
+                execute(message1);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
